044e1dd65c9e79e7d9567332dd19baadaa1a6eae
furky.eth <52575351+furkananter@users.noreply.github.com>
2021-11-22 11:06:21 +0300

    all files

---
+++ php-ders-3/baglanti.php
@@ -0,0 +1,4 @@
+<?php
+//baglanti.php
+$baglan = mysqli_connect("localhost","root","","hepsiburada");
+?>
---
+++ php-ders-3/index.php
@@ -0,0 +1,42 @@
+<?php
+require("baglanti.php");
+if($baglan){
+    $sorgu = mysqli_query($baglan,"SELECT * FROM musteriler");
+    /*
+    //mysqli_fetch_row kullanımı:
+    if(mysqli_num_rows($sorgu)>0){
+        $row = mysqli_fetch_row($sorgu);
+        echo $row[0];
+    }else{
+        echo "Sonuç Yok";
+    }
+    //fetch_array kullanımı:
+    if(mysqli_num_rows($sorgu)>0){
+        $row = $sorgu->fetch_array(MYSQLI_ASSOC);
+        echo $row["ID"];
+    }
+    */
+/* ----Örneklerden biri, nasıl yazdırılacağını gördük----
+    if(mysqli_num_rows($sorgu)>0){
+        while($row = mysqli_fetch_assoc($sorgu)){
+            echo "ID: ".$row["ID"]."Adı: ".$row["adi"]."Soyadı: ".$row["soyadi"]."Hesap No: ".$row["hesap_no"]."</br>";
+        }
+    }    
+*/
+echo "<table>";
+if(mysqli_num_rows($sorgu)>0){
+    while($row = mysqli_fetch_assoc($sorgu)){
+        echo "<tr>";
+            echo "<td>".$row["ID"]."</td>";
+            echo "<td>".$row["adi"]."</td>";
+        echo "</tr>";
+    }
+}
+echo "</table>";
+
+}else{
+    echo die("Veritabanına bağlantı sağlanamadı");
+}
+    
+    
+?>
